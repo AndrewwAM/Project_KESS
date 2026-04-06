d9ed3e5d9dc135dd0aa846a3daf5ce5136973395
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-04-06 14:02:37 -0400

    mejoras en el pathfinding

--- entities/enemies/base_enemy.gd
+++ entities/enemies/base_enemy.gd
@@ -5,7 +5,11 @@ extends CharacterBody2D
 @export var wobble_angle: float = 15.0
 @export var wobble_speed: float = 0.15
 
+@export var path_update_interval: float = 0.2
+var path_update_timer: float = 0.0
+
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 
 var player: Node2D = null
 var time_passed: float = 0.0
@@ -13,12 +17,34 @@ var time_passed: float = 0.0
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
 
+	# configuración pathfinding
+	nav_agent.path_desired_distance = 4.0
+	nav_agent.target_desired_distance = 4.0
+
 func _physics_process(_delta: float) -> void:
 	if player == null:
 		return
 
-	var direction: Vector2 = global_position.direction_to(player.global_position)
+<<<<<<< Updated upstream
+    var direction: Vector2 = global_position.direction_to(player.global_position)
+    velocity = direction * speed
+=======
+	# mejor pathfinding
+	path_update_timer += _delta
+	if path_update_timer >= path_update_interval:
+		nav_agent.target_position = player.global_position
+		path_update_timer = 0.0
+
+	if nav_agent.is_navigation_finished():
+		velocity = Vector2.ZERO
+		return
+
+	var next_path_position: Vector2 = nav_agent.get_next_path_position()
+
+	# movimiento a player
+	var direction: Vector2 = global_position.direction_to(next_path_position)
 	velocity = direction * speed
+>>>>>>> Stashed changes
 
 	move_and_slide()
 
