a27e482d49b2002952a40b631ec4dc9735b2426c
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-04-07 18:44:29 -0400

    menu menu meun

---
+++ ui/main_menu.gd
@@ -0,0 +1,48 @@
+extends Control
+
+@onready var start_button: TextureButton = $MainContainer/Layout/Buttons/StartButton
+@onready var credits_button: TextureButton = $MainContainer/Layout/Buttons/CreditsButton
+@onready var exit_button: TextureButton = $MainContainer/Layout/Buttons/ExitButton
+
+@onready var credits_panel: Control = $CreditsPanel
+@onready var close_credits_button: Button = $CreditsPanel/CloseCreditsButton
+
+@onready var click_sound: AudioStreamPlayer = $UIClickSound
+
+@export var game_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start_button.pressed.connect(_on_start_button_pressed)
+	credits_button.pressed.connect(_on_credits_button_pressed)
+	exit_button.pressed.connect(_on_exit_button_pressed)
+	close_credits_button.pressed.connect(_on_close_credits_button_pressed)
+
+	credits_panel.hide()
+
+
+func _on_start_button_pressed() -> void:
+	start_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+	click_sound.play()
+	await click_sound.finished
+
+	var error: int = get_tree().change_scene_to_packed(game_scene)
+	if error != OK:
+		printerr("Error al cargar la escena de juego. Código: ", error)
+
+func _on_credits_button_pressed() -> void:
+	click_sound.play()
+	credits_panel.show()
+
+func _on_close_credits_button_pressed() -> void:
+	click_sound.play()
+	credits_panel.hide()
+
+func _on_exit_button_pressed() -> void:
+	start_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+	click_sound.play()
+	await click_sound.finished
+
+	get_tree().quit()
